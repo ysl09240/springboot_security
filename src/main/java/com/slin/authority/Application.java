351ab7d55526bf4c8082486820c972d527ff8b71
yangsonglin <345978511@qq.com>
2018-11-13 09:19:25 +0800

    sloution chinese encode

--- src/main/java/com/slin/authority/Application.java
+++ src/main/java/com/slin/authority/Application.java
@@ -1,10 +1,18 @@
 package com.slin.authority;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 初始程序
  *
  * @author yangsonglin
  * @create 2018-11-12 17:54
  **/
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
